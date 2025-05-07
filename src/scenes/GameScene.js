98b9c98336b9963b48f6b97bb152b74aa4835fd0
Tonton-zinzin <anthony12041989@gmail.com>
2025-05-07 16:52:49 +0200

    Mise en place du sol en tileSprite avec réduction de la taille originale de mon image qui se répéte sur toute la largeur du canva

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -11,7 +11,7 @@ export default class GameScene extends Phaser.Scene {
     this.add.image(400, 300, "paysage");
     // this.add.tileSprite(0, 600 + 50, 800, 128, "ground").setOrigin(0, 1);
     this.add
-      // tileSprite(x => origine image sur canva, y => origine de Y (ici en bas du canva si hauteur = 600, largeur du rectangle d'affichage, hauteur rectangle affichage même si image =128 px (ici 128 pixel reduit à l'aide de setDisplaySize()), et ensuite ma key qui se situe dans preload))
+      // tileSprite(x => origine image sur canva, y => origine de Y (ici en bas du canva si hauteur = 600), largeur du rectangle d'affichage où mon image sera REPETER, hauteur rectangle affichage même si image =128 px (ici 128 pixel reduit à l'aide de setDisplaySize()), et ensuite ma key qui se situe dans preload))
       .tileSprite(0, 600, 800, 128, "ground")
       .setOrigin(0, 1)
       .setDisplaySize(800, 64);
